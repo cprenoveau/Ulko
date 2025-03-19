3d77e5a157323e622a0ec9e986aecb4315012fbc
Caroline Prenoveau <c.prenoveau@gmail.com>
2025-03-19 10:59:22 -0400

    Set black bars around cutscene

---
+++ Assets/Scripts/UI/ViewportBlackBars.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace Ulko.UI
+{
+    public class ViewportBlackBars : MonoBehaviour
+    {
+        public RectTransform topBar;
+        public RectTransform bottomBar;
+        public RectTransform leftBar;
+        public RectTransform rightBar;
+
+        private void Update()
+        {
+            var cam = Camera.main;
+
+            if(cam != null)
+            {
+                topBar.anchorMin = new Vector2(0, 1f - cam.rect.y);
+                bottomBar.anchorMax = new Vector2(1f, cam.rect.y);
+                leftBar.anchorMax = new Vector2(cam.rect.x, 1f);
+                rightBar.anchorMin = new Vector2(1f - cam.rect.x, 0);
+            }
+        }
+    }
+}
